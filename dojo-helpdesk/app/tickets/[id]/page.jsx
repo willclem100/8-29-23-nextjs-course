cc58fa6fdba3099fec6d69a01eb2436f9ed8375f
willclem100 <will.clem@biteninja.com>
2023-09-04 16:12:23 -0500

    Update to Lesson 12

--- dojo-helpdesk/app/tickets/[id]/page.jsx
+++ dojo-helpdesk/app/tickets/[id]/page.jsx
@@ -13,6 +13,9 @@ export async function getStaticParams() {
 }
 
 async function getTicket(id) {
+  //imitate delay
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+  
   const res = await fetch('http://localhost:4000/tickets/' + id, {
       next: {
           revalidate: 60
